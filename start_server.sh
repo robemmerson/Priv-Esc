c08eb0784937b44876aa34197555da51b798bd65
root <root@localhost.localdomain>
2018-05-15 22:58:49 +0100

    Fixed path and added dynamic IP in script

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echo "To download these files, please use the following command:"
+echo ""
+echo "wget -r http://$(ifconfig tun0 | grep netmask | awk '{ print $2 }')/shells"
+echo ""
+echo ""
+
 python -m SimpleHTTPServer 80
